44bd65ea0d1a4121022f16bf13d5809d9825d909
AyanCh0w <github@redbot.pro>
2024-10-19 13:28:15 -0400

    add google site index

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="google-site-verification" content="-hKZs65iX_vQhMu0eYth1FkSI8S0w2Mcv1aqD4UIFaE" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-blue-200`}
       >
